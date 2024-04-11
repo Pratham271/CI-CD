73cdcb15c1fd4bbfc9f6c36f7dbe47e5523ca15e
Pratham271 <chauhanpratham22@gmail.com>
2024-04-11 18:07:24 +0530

    updated the type again

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -71,7 +71,7 @@ export async function p2pTransfer(to:string, amount: number){
         })
     })
 
-    await prisma.$transaction(async (tx)=> {
+    await prisma.$transaction(async (tx:any) => {
         const fromBalance = await tx.balance.findUnique({
             where: {
                 userId: Number(from)
